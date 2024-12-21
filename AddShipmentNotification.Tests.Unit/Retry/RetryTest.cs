565f07ea5354cd0821caed310303b88a75b73503
Mark Standen <no-reply@rundog.dev>
2024-12-21 00:28:09 +0000

    Adds failing test to force implementation

--- AddShipmentNotification.Tests.Unit/Retry/RetryTest.cs
+++ AddShipmentNotification.Tests.Unit/Retry/RetryTest.cs
@@ -44,4 +44,30 @@ public class RetryTest
         Assert.True(result);
         returnsTrue.Verify(x => x(), Times.Exactly(3));
     }
+
+    [Fact]
+    public void Attempt_WithFunctionReturningFalseFalseFalse_LoopsThreeTimesReturnsFalse()
+    {
+        IRetry retry = new interview.Retry.Retry() { MaxRetries = 3 };
+        var returnsTrue = new Mock<Func<bool>>();
+        returnsTrue.SetupSequence(f => f()).Returns(false).Returns(false).Returns(false);
+
+        var result = retry.Attempt(returnsTrue.Object);
+
+        Assert.False(result);
+        returnsTrue.Verify(x => x(), Times.Exactly(3));
+    }
+
+    [Fact]
+    public void Attempt_WithTwoMaxRetries_LoopsTwiceReturnsFalse()
+    {
+        IRetry retry = new interview.Retry.Retry() { MaxRetries = 2 };
+        var returnsTrue = new Mock<Func<bool>>();
+        returnsTrue.SetupSequence(f => f()).Returns(false).Returns(false);
+
+        var result = retry.Attempt(returnsTrue.Object);
+
+        Assert.False(result);
+        returnsTrue.Verify(x => x(), Times.Exactly(2));
+    }
 }
