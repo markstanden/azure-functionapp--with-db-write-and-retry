8228ea2e799b723a235049c67c25d7d08d0b5b20
Mark Standen <no-reply@rundog.dev>
2024-12-21 00:18:09 +0000

    Adds failing test for initial failure, followed by secondary success

--- AddShipmentNotification.Tests.Unit/Retry/RetryTest.cs
+++ AddShipmentNotification.Tests.Unit/Retry/RetryTest.cs
@@ -18,4 +18,17 @@ public class RetryTest
         Assert.True(result);
         returnsTrue.Verify(x => x(), Times.Once);
     }
+
+    [Fact]
+    public void Attempt_WithFunctionReturningFalseTrue_LoopsTwice()
+    {
+        IRetry retry = new interview.Retry.Retry() { MaxRetries = 3 };
+        var returnsTrue = new Mock<Func<bool>>();
+        returnsTrue.SetupSequence(f => f()).Returns(false).Returns(true);
+
+        var result = retry.Attempt(returnsTrue.Object);
+
+        Assert.True(result);
+        returnsTrue.Verify(x => x(), Times.Exactly(2));
+    }
 }
