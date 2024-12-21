19daeaa43f5961f02f90bc2f816ab020b4a0d571
Mark Standen <no-reply@rundog.dev>
2024-12-21 00:23:03 +0000

    Failing test for third loop

--- AddShipmentNotification.Tests.Unit/Retry/RetryTest.cs
+++ AddShipmentNotification.Tests.Unit/Retry/RetryTest.cs
@@ -7,7 +7,7 @@ namespace AddShipmentNotification.Tests.Unit.Retry;
 public class RetryTest
 {
     [Fact]
-    public void Attempt_WithFunctionReturningTrue_LoopsOnce()
+    public void Attempt_WithFunctionReturningTrue_LoopsOnceRetunsTrue()
     {
         IRetry retry = new interview.Retry.Retry() { MaxRetries = 3 };
         var returnsTrue = new Mock<Func<bool>>();
@@ -20,7 +20,7 @@ public class RetryTest
     }
 
     [Fact]
-    public void Attempt_WithFunctionReturningFalseTrue_LoopsTwice()
+    public void Attempt_WithFunctionReturningFalseTrue_LoopsTwiceReturnsTrue()
     {
         IRetry retry = new interview.Retry.Retry() { MaxRetries = 3 };
         var returnsTrue = new Mock<Func<bool>>();
@@ -30,5 +30,18 @@ public class RetryTest
 
         Assert.True(result);
         returnsTrue.Verify(x => x(), Times.Exactly(2));
+    }
+
+    [Fact]
+    public void Attempt_WithFunctionReturningFalseFalseTrue_LoopsThreeTimesReturnsTrue()
+    {
+        IRetry retry = new interview.Retry.Retry() { MaxRetries = 3 };
+        var returnsTrue = new Mock<Func<bool>>();
+        returnsTrue.SetupSequence(f => f()).Returns(false).Returns(false).Returns(true);
+
+        var result = retry.Attempt(returnsTrue.Object);
+
+        Assert.True(result);
+        returnsTrue.Verify(x => x(), Times.Exactly(3));
     }
 }
