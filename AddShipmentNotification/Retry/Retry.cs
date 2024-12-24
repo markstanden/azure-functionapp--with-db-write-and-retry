664281e6bac7141f5bd887d36d5a350b6a63281b
Mark Standen <no-reply@rundog.dev>
2024-12-24 11:14:51 +0000

    Implements delay method

--- AddShipmentNotification/Retry/Retry.cs
+++ AddShipmentNotification/Retry/Retry.cs
@@ -2,6 +2,8 @@ namespace interview.Retry;
 
 public class Retry : IRetry
 {
+    public int DelaySeconds = 10;
+    public Action<TimeSpan> DelayFn { get; init; } = (TimeSpan timeSpan) => Task.Delay(timeSpan);
     public int MaxRetries { get; init; } = 3;
 
     public bool Attempt(Func<bool> fn, int attempt = 1)
@@ -15,6 +17,8 @@ public class Retry : IRetry
         {
             return false;
         }
+
+        DelayFn(TimeSpan.FromSeconds(DelaySeconds));
 
         // Recursively call with an incremented attempt number
         return Attempt(fn, attempt + 1);
