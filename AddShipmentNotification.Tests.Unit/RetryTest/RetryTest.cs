049b97d30c4dcf39ebe87d2c599b40a721f3febe
Mark Standen <no-reply@rundog.dev>
2025-01-02 21:59:52 +0000

    Uses delegate Func to replace boilerplate on Retry class initiations

--- AddShipmentNotification.Tests.Unit/RetryTest/RetryTest.cs
+++ AddShipmentNotification.Tests.Unit/RetryTest/RetryTest.cs
@@ -13,6 +13,10 @@ public class RetryTest
     private const int ThirdAttempt = 3;
     private const int FourthAttempt = 4;
 
+    // Convenience delegate to reduce boilerplate within tests
+    private readonly Func<int, IRetry> _instantRetry = (attempts) =>
+        new Retry { MaxRetries = attempts, DelaySeconds = 0 };
+
     private Mock<Func<TimeSpan, Task>> CreateMockDelayFn()
     {
         return new Mock<Func<TimeSpan, Task>>();
@@ -22,7 +26,7 @@ public class RetryTest
     public async Task Attempt_WithFunctionReturningTrue_LoopsOnceReturnsTrue()
     {
         var fakeRetryTaskFn = FakeRetryTaskFn.CreateSuccessOn(FirstAttempt);
-        IRetry retry = new Retry() { MaxRetries = 3, DelaySeconds = 0 };
+        IRetry retry = _instantRetry(3);
 
         var result = await retry.Attempt(fakeRetryTaskFn.RetryTask);
 
@@ -34,7 +38,7 @@ public class RetryTest
     public async Task Attempt_WithFunctionReturningFalseTrue_LoopsTwiceReturnsTrue()
     {
         var fakeRetryTaskFn = FakeRetryTaskFn.CreateSuccessOn(SecondAttempt);
-        IRetry retry = new Retry() { MaxRetries = 3, DelaySeconds = 0 };
+        IRetry retry = _instantRetry(3);
 
         var result = await retry.Attempt(fakeRetryTaskFn.RetryTask);
 
@@ -46,7 +50,7 @@ public class RetryTest
     public async Task Attempt_WithFunctionReturningFalseFalseTrue_LoopsThreeTimesReturnsTrue()
     {
         var fakeRetryTaskFn = FakeRetryTaskFn.CreateSuccessOn(ThirdAttempt);
-        IRetry retry = new Retry() { MaxRetries = 3, DelaySeconds = 0 };
+        IRetry retry = _instantRetry(3);
 
         var result = await retry.Attempt(fakeRetryTaskFn.RetryTask);
 
@@ -58,7 +62,7 @@ public class RetryTest
     public async Task Attempt_WithFunctionReturningFalseFalseFalse_LoopsThreeTimesReturnsFalse()
     {
         var fakeRetryTaskFn = FakeRetryTaskFn.CreateSuccessOn(FourthAttempt);
-        IRetry retry = new Retry() { MaxRetries = 3, DelaySeconds = 0 };
+        IRetry retry = _instantRetry(3);
 
         var result = await retry.Attempt(fakeRetryTaskFn.RetryTask);
 
@@ -70,7 +74,7 @@ public class RetryTest
     public async Task Attempt_WithTwoMaxRetries_LoopsTwiceReturnsFalse()
     {
         var fakeRetryTaskFn = FakeRetryTaskFn.CreateSuccessOn(ThirdAttempt);
-        IRetry retry = new Retry() { MaxRetries = 2, DelaySeconds = 0 };
+        IRetry retry = _instantRetry(2);
 
         var result = await retry.Attempt(fakeRetryTaskFn.RetryTask);
 
