6f6c9e8d50686566a2521cf938f58a11f3c6b1fe
Mark Standen <no-reply@rundog.dev>
2025-01-04 21:29:07 +0000

    Fixes potential null dependencies in Program.cs, adds sensible defaults.

--- AddShipmentNotification/Program.cs
+++ AddShipmentNotification/Program.cs
@@ -23,23 +23,25 @@ builder.Services.AddSingleton<ISanitation, Sanitation>();
 // Adds Http Client as a DI Constructor parameter to be injected into HttpClientWrapper
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<IHttpClientWrapper, HttpClientWrapper>(
-    serviceProvider => new HttpClientWrapper(serviceProvider.GetService<HttpClient>())
+    serviceProvider => new HttpClientWrapper(serviceProvider.GetRequiredService<HttpClient>())
 );
 
 // Adds a Sql connector singleton, for dependency injection into SqlDbService
 builder.Services.AddSingleton<IDbConnector, SqlConnector>(serviceProvider => new SqlConnector(
-    builder.Configuration.GetValue<string>("SqlConnectionString") ?? "SqlConnectionString_NOT_SET"
+    builder.Configuration.GetValue<string>("SqlConnectionString")
+        ?? throw new InvalidOperationException(
+            "SqlConnectionString is a required configuration value."
+        )
 ));
 
 // Adds DqlConnection dependency to be injected into SqlDbService
 builder.Services.AddSingleton<ISqlDbService, SqlDbService>(serviceProvider => new SqlDbService(
-    serviceProvider.GetService<IDbConnector>(),
+    serviceProvider.GetRequiredService<IDbConnector>(),
     serviceProvider.GetRequiredService<ISanitation>(),
     serviceProvider.GetRequiredService<ILogger<SqlDbService>>(),
-    builder.Configuration.GetValue<string>("dbName") ?? "dbName_NOT_SET",
-    builder.Configuration.GetValue<string>("shipmentTableName") ?? "shipmentTableName_NOT_SET",
-    builder.Configuration.GetValue<string>("shipmentLinesTableName")
-        ?? "shipmentLinesTableName_NOT_SET"
+    builder.Configuration.GetValue<string>("dbSchema") ?? "dbo",
+    builder.Configuration.GetValue<string>("shipmentTableName") ?? "markShipment",
+    builder.Configuration.GetValue<string>("shipmentLinesTableName") ?? "markShipment_line"
 ));
 
 builder.Build().Run();
