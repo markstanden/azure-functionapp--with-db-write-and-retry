a4a6c8c424d7a0f87dfe653c1d475c67134a5f03
Mark Standen <no-reply@rundog.dev>
2025-01-05 16:18:50 +0000

    Updates SQL lookup query with LIKE to narrow to just required additions

--- AddShipmentNotification.Tests.Unit/__SetupAndTesting/sql/db_list.sql
+++ AddShipmentNotification.Tests.Unit/__SetupAndTesting/sql/db_list.sql
@@ -3,8 +3,7 @@ SELECT
     shipments.shipmentDate,
     lines.sku,
     lines.quantity
-FROM
-    (SELECT * FROM dbo.markShipment) AS shipments
-        INNER JOIN
-    (SELECT * FROM dbo.markShipment_Line) AS lines
-    ON shipments.shipmentId = lines.shipmentId;
+FROM dbo.markShipment AS shipments
+         INNER JOIN dbo.markShipment_Line AS lines
+                    ON shipments.shipmentId = lines.shipmentId
+WHERE shipments.shipmentId LIKE '%2025-01-06%';
