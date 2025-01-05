7909fb95869007c3aaf5386aca5fc958ef4002e8
Mark Standen <no-reply@rundog.dev>
2025-01-05 15:22:39 +0000

    Updated Sql Setup with foreign key constraint on shipmentId

--- AddShipmentNotification.Tests.Unit/__SetupAndTesting/sql/database_init.sql
+++ AddShipmentNotification.Tests.Unit/__SetupAndTesting/sql/database_init.sql
@@ -1,14 +1,17 @@
 DROP TABLE IF EXISTS [dbo].[markShipment];
 DROP TABLE IF EXISTS [dbo].[markShipment_Line];
 
-CREATE TABLE dbo.markShipment (
-                                  [shipmentId] VARCHAR(255) NOT NULL PRIMARY KEY,
+CREATE TABLE [dbo].[markShipment]
+(
+    [shipmentId] VARCHAR(255) NOT NULL PRIMARY KEY,
     [shipmentDate] DATETIME NOT NULL
 );
 
-CREATE TABLE dbo.markShipment_Line (
+CREATE TABLE [dbo].[markShipment_Line]
+(
     [Id] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [sku] VARCHAR(255) NOT NULL,
     [quantity] INT NOT NULL,
 	[shipmentId] VARCHAR(255) NOT NULL
+        FOREIGN KEY ([shipmentId]) REFERENCES [dbo].[markShipment] ([shipmentId])
 );
