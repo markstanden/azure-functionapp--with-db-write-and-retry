7b792fa06cca1578b63006b410c5cfcf381bf938
Mark Standen <no-reply@rundog.dev>
2025-01-05 16:25:41 +0000

    Updates query with variable to simplify logic

--- AddShipmentNotification.Tests.Unit/__SetupAndTesting/sql/db_list.sql
+++ AddShipmentNotification.Tests.Unit/__SetupAndTesting/sql/db_list.sql
@@ -2,6 +2,11 @@
   creates an inner join between the two tables and displays the results with
   a shipmentId containing the required text
  */
+
+-- Replace the string here with the shipmentId search term 
+DECLARE
+@SearchShipmentId VARCHAR(255) = '2025-01-06';
+
 SELECT
     shipments.shipmentId,
     shipments.shipmentDate,
@@ -10,4 +15,4 @@ SELECT
 FROM dbo.markShipment AS shipments
          INNER JOIN dbo.markShipment_Line AS lines
                     ON shipments.shipmentId = lines.shipmentId
-WHERE shipments.shipmentId LIKE '%2025-01-06%';
+WHERE shipments.shipmentId LIKE CONCAT('%', @SearchShipmentId, '%');
