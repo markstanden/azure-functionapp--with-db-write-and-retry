ab37a318aa89250a814e5a5a71af7c22ecbe0e1a
Mark Standen <no-reply@rundog.dev>
2025-01-13 13:55:08 +0000

    Removed redundant [Required] attributes

--- AddShipmentNotification/Models/Domain/ShipmentLine.cs
+++ AddShipmentNotification/Models/Domain/ShipmentLine.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace AddShipmentNotification.Models.Domain
 {
     /// <summary>
@@ -7,21 +5,18 @@ namespace AddShipmentNotification.Models.Domain
     /// <c>ShipmentNotification</c> sent from a provider, and is used for
     /// JSON deserialization of the servicebus message
     /// </summary>
-    [Serializable]
     public class ShipmentLine
     {
         /// <summary>
         /// For serialisation and sanitation purposes I have assumed the
-        /// Sku is a alphanumeric string only.
+        /// Sku is an alphanumeric string only.
         /// Sanitisation will remove all special characters and whitespace.
         /// </summary>
-        [Required]
         public required string sku { get; init; }
 
         /// <summary>
         /// JSON serialisation will throw if the supplied value is not an integer
         /// </summary>
-        [Required]
         public required int quantity { get; init; }
     }
 }
--- AddShipmentNotification/Models/Domain/ShipmentNotification.cs
+++ AddShipmentNotification/Models/Domain/ShipmentNotification.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace AddShipmentNotification.Models.Domain
 {
     /// <summary>
@@ -7,28 +5,24 @@ namespace AddShipmentNotification.Models.Domain
     /// sent from a provider, and is used for JSON deserialization of the
     /// servicebus message
     /// </summary>
-    [Serializable]
     public class ShipmentNotification
     {
         /// <summary>
         /// For serialisation and sanitation purposes I have assumed the
-        /// Shipment is a alphanumeric string that may contain hyphens.
+        /// ShipmentId is an alphanumeric string that may contain hyphens.
         /// Sanitisation will remove all other special characters and whitespace.
         /// </summary>
-        [Required]
         public required string shipmentId { get; init; }
 
         /// <summary>
         /// JSON serialisation will throw if the supplied value is not a valid datetime string
         /// </summary>
-        [Required]
         public required DateTime shipmentDate { get; init; }
 
         /// <summary>
         /// The order lines within the notification.
         /// Assumed as required as a shipment will always contain items to deliver.
         /// </summary>
-        [Required]
         public required ShipmentLine[] shipmentLines { get; init; }
     }
 }
